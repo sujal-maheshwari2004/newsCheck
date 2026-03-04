e4eab8187596f5153ae8e72b4a8c23e4481a83c6
Sujal Maheshwari <sujalmaheshwari07@gmail.com>
2026-03-04 22:26:03 +0530

    Update FFmpeg installation process in build script

--- backend/build.sh
+++ backend/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
-echo "Installing FFmpeg static build..."
+echo "Downloading FFmpeg static build..."
 
 curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
 
@@ -9,10 +9,18 @@ tar -xf ffmpeg.tar.xz
 
 FFMPEG_DIR=$(find . -type d -name "ffmpeg-*-amd64-static")
 
-cp $FFMPEG_DIR/ffmpeg /usr/local/bin/
-cp $FFMPEG_DIR/ffprobe /usr/local/bin/
+echo "Setting up FFmpeg..."
 
-echo "FFmpeg installed."
+mkdir -p ffmpeg-bin
+cp $FFMPEG_DIR/ffmpeg ffmpeg-bin/
+cp $FFMPEG_DIR/ffprobe ffmpeg-bin/
+
+chmod +x ffmpeg-bin/ffmpeg
+chmod +x ffmpeg-bin/ffprobe
+
+export PATH="$PWD/ffmpeg-bin:$PATH"
+
+echo "FFmpeg installed locally."
 
 echo "Upgrading pip..."
 pip install --upgrade pip
